acf5c508cca6f795adfb6f3b5de17b34c232da95
cptWOLF01 <himanshum9199@gmail.com>
2026-07-01 20:36:29 +0530

    Time: 2 ms (99.81%) | Memory: 69.4 MB (79.36%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+
+        int windowSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            windowSum = windowSum + nums[i];
+        }
+
+        int maxSum = windowSum;
+
+        for (int i = k; i < nums.length; i++) {
+
+            windowSum = windowSum - nums[i - k];
+
+            windowSum = windowSum + nums[i];
+
+            if (windowSum > maxSum) {
+                maxSum = windowSum;
+            }
+        }
+        return (double) maxSum / k;
+    }
+}
