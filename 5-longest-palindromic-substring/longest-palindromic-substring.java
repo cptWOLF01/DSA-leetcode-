5c10e4a622e629e1ee57176de9cb75caef1d8bd1
cptWOLF01 <himanshum9199@gmail.com>
2026-07-17 20:25:32 +0530

    Time: 1568 ms (11.04%) | Memory: 43.3 MB (89.59%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,37 +1,33 @@
 class Solution {
-
-    int start = 0;
-    int end = 0;
-
-    public String longestPalindrome(String s) {
-
-        for (int i = 0; i < s.length(); i++) {
-
-            expand(s, i, i);       // Odd length
-
-            expand(s, i, i + 1);   // Even length
+    int max=0, start=0, end=0;
+    public boolean isPalindrome(String s, int i, int j){
+        while (i<j){
+            char ch1=s.charAt(i);
+            char ch2=s.charAt(j);
+            if(ch1!=ch2){
+                return false;
+            }
+            i++;
+            j--;
         }
-
-        return s.substring(start, end + 1);
+        return true;
     }
-
-    public void expand(String s, int left, int right) {
-
-        while (left >= 0 &&
-               right < s.length() &&
-               s.charAt(left) == s.charAt(right)) {
-
-            left--;
-            right++;
+    public String longestPalindrome(String s) {
+        int n=s.length();
+        for (int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j)==true)
+                {
+                    if ((j - i + 1) > max)
+                    {
+                        max=j-i+1;
+                        start=i;
+                        end=j;
+                    }
+                }
+            }
         }
-
-        // Move back to the last valid palindrome
-        left++;
-        right--;
-
-        if (right - left > end - start) {
-            start = left;
-            end = right;
-        }
+        return s.substring(start,end+1);
+        
     }
 }
