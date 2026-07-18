de02ac2d7caca7eda9dff5fa1041fb3556c65b02
cptWOLF01 <himanshum9199@gmail.com>
2026-07-18 23:30:03 +0530

    Time: 4 ms (82.16%) | Memory: 44.4 MB (67.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-
-        if (s.length() != t.length()) {
+        if(s.length() != t.length()){
             return false;
         }
 
-        int[] count = new int[26];
+        int [] count = new int[26];
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i <s.length() ; i ++){
             count[s.charAt(i) - 'a']++;
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            if (--count[t.charAt(i) - 'a'] < 0) {
+        for(int i = 0; i <t.length() ; i ++){
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
                 return false;
             }
         }
