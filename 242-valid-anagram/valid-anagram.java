28164f05e6840dedc23fee2e415cd1bbeeeafc53
cptWOLF01 <himanshum9199@gmail.com>
2026-07-18 23:15:32 +0530

    Time: 3 ms (93.33%) | Memory: 44 MB (95.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -9,11 +9,10 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
         }
 
-        for (int num : count) {
-            if (num != 0) {
+        for (int i = 0; i < t.length(); i++) {
+            if (--count[t.charAt(i) - 'a'] < 0) {
                 return false;
             }
         }
