dec1f66070bf4bf65fc1b6569f359b8b9f4ceca2
cptWOLF01 <himanshum9199@gmail.com>
2026-07-19 18:01:05 +0530

    Time: 3 ms (41.28%) | Memory: 43.2 MB (53.63%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -21,26 +21,6 @@ class Solution {
                 return (char)(i + 'a');
             }
         }
-
-        }
-
-        else{
-             for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-        }
-
-        // Remove letters of s
-        for (int i = 0; i < t.length(); i++) {
-            count[t.charAt(i) - 'a']--;
-        }
-
-        // Find the remaining letter
-        for (int i = 0; i < 26; i++) {
-            if (count[i] > 0) {
-                return (char)(i + 'a');
-            }
-        }
-
         }
         return ' ';
     }
