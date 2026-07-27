63ad196da9f69a3c0afc9a39a1968aba8129466c
cptWOLF01 <himanshum9199@gmail.com>
2026-07-27 23:15:12 +0530

    Time: 79 ms (12.91%) | Memory: 106.9 MB (50.33%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -10,18 +10,14 @@ class Solution {
 
         for (int i = n - 1; i >= 0; i--) {
 
-            while (!helper.isEmpty()
-                    && temperatures[helper.peek()] <= temperatures[i]) {
+            while (!helper.isEmpty()  && temperatures[helper.peek()] <= temperatures[i]) {
 
                 helper.pop();
             }
 
-            if (helper.isEmpty()) {
-                ans[i] = 0;
-            } else {
-                ans[i] = helper.peek() - i;
-            }
-
+           if(!helper.isEmpty()){
+            ans[i]=helper.peek()-i;
+           }
             helper.push(i);
         }
 
