63b2b8e2b41c1b40e34cd74c91963cde7be6d525
cptWOLF01 <himanshum9199@gmail.com>
2026-07-27 23:00:36 +0530

    Time: 78 ms (18.43%) | Memory: 106.7 MB (67.46%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+
+        int[] ans = new int[n];
+
+        Stack<Integer> helper = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!helper.isEmpty()
+                    && temperatures[helper.peek()] <= temperatures[i]) {
+
+                helper.pop();
+            }
+
+            if (helper.isEmpty()) {
+                ans[i] = 0;
+            } else {
+                ans[i] = helper.peek() - i;
+            }
+
+            helper.push(i);
+        }
+
+        return ans;
+    }
+}
