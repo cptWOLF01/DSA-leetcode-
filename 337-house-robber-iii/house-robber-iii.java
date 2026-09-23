8e4dd2e298b2a8f594ab369912a8d4316d9cc535
cptWOLF01 <himanshum9199@gmail.com>
2026-09-23 16:56:03 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (58.86%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int rob(TreeNode root) {
+
+        int[] result = helper(root);
+
+        return Math.max(result[0], result[1]);
+    }
+
+    public int[] helper(TreeNode root) {
+
+        if (root == null) {
+            return new int[]{0, 0};
+        }
+
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+
+        // Rob current node
+        int rob = root.val + left[1] + right[1];
+
+        // Don't rob current node
+        int notRob = Math.max(left[0], left[1])
+                   + Math.max(right[0], right[1]);
+
+        return new int[]{rob, notRob};
+    }
+}
